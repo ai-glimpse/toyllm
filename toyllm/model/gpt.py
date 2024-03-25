7672a73ccb517dd8f790df54302a55ed53a1390a
Xiangzhuang Shen <datahonor@gmail.com>
2024-03-25 22:06:46 +0800

    change names

--- toyllm/model/gpt.py
+++ toyllm/model/gpt.py
@@ -184,29 +184,29 @@ class GPTModel(nn.Module):
         return logits
 
 
-def generate_text_simple(model: GPTModel, idx, max_new_tokens, context_size):
+def generate_text_simple(model: GPTModel, text_token_ids: torch.Tensor, max_new_tokens: int, ctx_len: int):
     # idx is (B, T) array of indices in the current context
     for _ in range(max_new_tokens):
-        # Crop current context if it exceeds the supported context size
+        # Crop current context if it exceeds the supported context size(ctx_len)
         # E.g., if LLM supports only 5 tokens, and the context size is 10
         # then only the last 5 tokens are used as context
-        idx_cond = idx[:, -context_size:]
+        context_text_token_ids = text_token_ids[:, -ctx_len:]
 
         # Get the predictions
         with torch.no_grad():
-            logits = model(idx_cond)
+            logits = model(context_text_token_ids)
 
         # Focus only on the last time step
         # (batch, n_token, vocab_size) becomes (batch, vocab_size)
         logits = logits[:, -1, :]
 
         # Get the idx of the vocab entry with the highest logits value
-        idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
+        next_token_id = torch.argmax(logits, dim=-1, keepdim=True)  # (batch, 1)
 
         # Append sampled index to the running sequence
-        idx = torch.cat((idx, idx_next), dim=1)  # (batch, n_tokens+1)
+        text_token_ids = torch.cat((text_token_ids, next_token_id), dim=1)  # (batch, n_tokens+1)
 
-    return idx
+    return text_token_ids
 
 
 if __name__ == "__main__":
@@ -228,17 +228,17 @@ if __name__ == "__main__":
     start_context = "Hello, I am"
 
     tokenizer = get_gpt2_tokenizer()
-    encoded_tensor = text_to_token_ids(start_context).to(device)
+    text_token_ids = text_to_token_ids(start_context).to(device)
 
     print(f"\n{50*'='}\n{22*' '}IN\n{50*'='}")
     print("\nInput text:", start_context)
-    print("encoded_tensor.shape:", encoded_tensor.shape)
+    print("encoded_tensor.shape:", text_token_ids.shape)
 
     out = generate_text_simple(
         model=model,
-        idx=encoded_tensor,
+        text_token_ids=text_token_ids,
         max_new_tokens=10,
-        context_size=GPT_CONFIG_124M.ctx_len,
+        ctx_len=GPT_CONFIG_124M.ctx_len,
     )
     decoded_text = token_ids_to_text(out)
 
