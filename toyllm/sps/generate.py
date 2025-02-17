b24a1610e56d413d63f3beeae1257d59e5ec80c1
Xiangzhuang Shen <datahonor@gmail.com>
2025-02-17 17:33:38 +0800

    fix: bugs

--- toyllm/sps/generate.py
+++ toyllm/sps/generate.py
@@ -23,7 +23,6 @@ class TextGenerator:
         self,
         prompt_text: str,
         min_gen_tokens: int = 100,
-        top_k: Optional[int] = None,
         temperature: Optional[float] = None,
     ) -> str:
         context_length = self.target_model.get_context_length()
@@ -40,24 +39,25 @@ class TextGenerator:
                 # Draft model lookahead: (prompt_tokens, next_token_id, next_token_logits)
                 lookahead_tuples = []
                 draft_prompt_tokens = prompt_tokens.clone()
-                for _ in range(self.lookahead + 1):
+                for _ in range(self.lookahead):
                     draft_next_token_ids, draft_next_token_logits = self._get_logits_and_next_token_id(
-                        draft_prompt_tokens, context_length, self.draft_model, top_k, temperature
+                        draft_prompt_tokens, context_length, self.draft_model, temperature,
                     )
                     draft_next_token_id, draft_next_token_logits = draft_next_token_ids[0], draft_next_token_logits[0]
-                    lookahead_tuples.append((prompt_tokens, draft_next_token_id, draft_next_token_logits))
+                    lookahead_tuples.append((draft_prompt_tokens.clone(), draft_next_token_id, draft_next_token_logits))
                     # Append sampled index to the running sequence
                     # (batch, n_tokens') --(append next token)--> (batch, n_tokens' + 1)
                     draft_prompt_tokens = torch.cat((draft_prompt_tokens, draft_next_token_ids), dim=1)
-                # Target model one step
-                batch_prompt_tokens = torch.cat([t[0] for t in lookahead_tuples], dim=0)
-                _, batch_target_next_token_logits = self._get_logits_and_next_token_id(
-                    batch_prompt_tokens, context_length, self.target_model, top_k, temperature
-                )
+                
+                # Target model logits
+                target_model_probs = self._get_latest_n_token_probs(
+                    draft_prompt_tokens, self.lookahead + 1, context_length, self.target_model, temperature
+                ).squeeze(0)
+                
                 # Compare the target model's next token with the draft model's lookahead
-                for t in range(self.lookahead + 1):
+                for t in range(self.lookahead):
                     draft_next_token_id, draft_next_token_logits = lookahead_tuples[t][1], lookahead_tuples[t][2]
-                    target_next_token_logits = batch_target_next_token_logits[t, :]
+                    target_next_token_logits = target_model_probs[t, :]
                     
                     r = np.random.rand()
                     if r < min(1.0, (target_next_token_logits[draft_next_token_id] / draft_next_token_logits[draft_next_token_id]).cpu().item()):
@@ -86,8 +86,8 @@ class TextGenerator:
         prompt_tokens: torch.Tensor,
         context_length: int,
         model: BaseSpsModel,
-        top_k: Optional[int] = None,
         temperature: Optional[float] = None,
+        eps: float = 1e-8,
     ) -> tuple[torch.Tensor, torch.Tensor]:
         # Crop current context if it exceeds the supported context size(ctx_len)
         # E.g., if LLM supports only 5 tokens, and the context size is 10
@@ -98,43 +98,36 @@ class TextGenerator:
         logits = model.get_next_token_logits(prompt_token=context_text_token_ids)
         logits = logits[:, -1, :]
         
-        if top_k is not None:
-            logits = self._logits_top_k_filter(logits, top_k)
         if temperature is not None:
-            probs = self._logits_temperature_scale(logits, temperature)
+            logits = logits / (temperature + eps)
+            probs = torch.softmax(logits, dim=-1)
             next_token_id = torch.multinomial(probs, num_samples=1)
         else:
             next_token_id = torch.argmax(logits, dim=-1, keepdim=True)
+            probs = torch.softmax(logits, dim=-1)
+
+        return next_token_id, probs
+
+    def _get_latest_n_token_probs(
+        self,
+        prompt_tokens: torch.Tensor,
+        n: int, 
+        context_length: int,
+        model: BaseSpsModel,
+        temperature: Optional[float] = None,
+        eps: float = 1e-8,
+    ) -> torch.Tensor:
+        # Crop current context if it exceeds the supported context size(ctx_len)
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then only the last 5 tokens are used as context
         
-        return next_token_id, logits
+        # (batch, n_tokens) --(crop context)--> (batch, n_tokens' = min(ctx_len, n_tokens))
+        context_text_token_ids = prompt_tokens[:, -context_length :]
+        logits = model.get_next_token_logits(prompt_token=context_text_token_ids)
+        logits = logits[:, -n:, :]
 
-    
-    @jaxtyping.jaxtyped(typechecker=typechecker)
-    @staticmethod
-    def _logits_top_k_filter(
-        logits: jaxtyping.Float[torch.Tensor, "batch_size vocab_size"],
-        top_k: int,
-    ) -> jaxtyping.Float[torch.Tensor, "batch_size vocab_size"]:
-        """
-        ref1: https://github.com/rasbt/LLMs-from-scratch/blob/62fb11d5e0449a6d49bda7337d6cfa5a735718da/ch05/01_main-chapter-code/generate.py#L166-L185
-        ref2: https://github.com/huggingface/transformers/blob/c4d4e8bdbd25d9463d41de6398940329c89b7fb6/src/transformers/generation_utils.py#L903-L941
-        ref3: https://github.com/meta-llama/llama/blob/main/llama/generation.py#L188-L192
-        """
-        top_k = min(top_k, logits.size(-1))  # make sure top_k <= vocab size
-        top_k_logits, _top_k_indexes = torch.topk(logits, k=top_k, dim=-1)
-        min_logit = top_k_logits[:, -1]
-        logits = torch.where(logits < min_logit, torch.tensor(float("-inf")).to(logits.device), logits)
-        return logits
-
-
-    @jaxtyping.jaxtyped(typechecker=typechecker)
-    @staticmethod
-    def _logits_temperature_scale(
-        logits: jaxtyping.Float[torch.Tensor, "batch_size vocab_size"],
-        temperature: float,
-        eps: float = 1e-10,
-    ) -> jaxtyping.Float[torch.Tensor, "batch_size vocab_size"]:
-        logits = logits / (temperature + eps)
+        if temperature is not None:
+            logits = logits / (temperature + eps)
         probs = torch.softmax(logits, dim=-1)
         return probs
 
