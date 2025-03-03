7050dc64e5723129906c57a39f8765ecd05297c8
Xiangzhuang Shen <datahonor@gmail.com>
2025-03-03 21:48:14 +0800

    add: paper algo statements

--- toyllm/sps/generate.py
+++ toyllm/sps/generate.py
@@ -10,9 +10,9 @@ from toyllm.sps.models import BaseSpsModel
 @dataclass
 class SpsTextGenerator:
     tokenizer: tiktoken.Encoding
-    target_model: BaseSpsModel
-    draft_model: BaseSpsModel
-    lookahead: int = 5
+    target_model: BaseSpsModel  # auto-regressive target model
+    draft_model: BaseSpsModel  # auto-regressive draft model
+    lookahead: int = 5  # K in sps paper
     seed: int = 42
 
     def __post_init__(self):
@@ -20,7 +20,7 @@ class SpsTextGenerator:
 
     def generate(
         self,
-        prompt: str,
+        prompt: str,  # given initial prompt
         target_seq_len: int = 100,
         temperature: None | float = None,
     ) -> str:
@@ -31,57 +31,61 @@ class SpsTextGenerator:
             The target sequence length to generate.
             temperature: Used to control the randomness of the generated text.
         """
+
+        # sequence x0, x1, ..., xt
         text_id_list = self.tokenizer.encode(prompt)
         prompt_tokens = torch.tensor(text_id_list).to(self.target_model.device())
 
+        # Initialise n <- t
         n = prompt_tokens.size(0)
+        # while n < T do
         while n < target_seq_len:
             with torch.inference_mode():
                 draft_prompt_tokens = prompt_tokens.clone()
+                # for t = 1:K do
+                #   Sample draft auto-regressively x'_t ~ p(x|x1, ..., xn, x'_1, ..., x'_{t-1})
                 for _ in range(self.lookahead):
                     draft_model_probs = self.draft_model.forward(draft_prompt_tokens, temperature)
                     next_token_id = torch.multinomial(draft_model_probs[-1], num_samples=1)
-                    # Append sampled index to the running sequence
                     draft_prompt_tokens = torch.cat([draft_prompt_tokens, next_token_id], dim=0)
 
-                target_model_probs = self.target_model.forward(draft_prompt_tokens, temperature)
-
-                # 仅仅考虑最后 (lookahead + 1) 步的 token 预测概率
+                # 仅考虑 draft model 生成的 token
                 draft_generate_tokens = draft_prompt_tokens[-self.lookahead :]
                 draft_model_probs = draft_model_probs[-self.lookahead :, :]
+
+                # compute k+1 sets of logits from drafts x'_1, ..., x'_K
                 # target 模型另外考虑最后一个 token 的预测，用于 All accept 时推断出下一个 token
+                target_model_probs = self.target_model.forward(draft_prompt_tokens, temperature)
                 target_model_probs = target_model_probs[-(self.lookahead + 1) :, :]
 
                 all_accept = True
-                # Compare the target model's next token with the draft model's lookahead
+                # for t = 1:K do
                 for t in range(self.lookahead):
+                    # Sample r ~ U(0, 1) from a uniform distribution
+                    r = self.rng.random()
+
                     x = draft_generate_tokens[t]
                     px = draft_model_probs[t, x]
                     qx = target_model_probs[t, x]
 
-                    r = self.rng.random()
+                    # if r < min(1, q(x) / p(x)), then (accept x)
                     if r < min(1.0, (qx / px).cpu().item()):
-                        print("Accept!")
                         next_generated_token = x
-                        # Update prompt tokens
                         prompt_tokens = torch.cat([prompt_tokens, next_generated_token.unsqueeze(0)], dim=0)
+                        n += 1
                     else:
                         all_accept = False
-                        print("Reject!")
                         prob_diff = target_model_probs[t] - draft_model_probs[t]
                         # element-wise max to 0
                         prob_diff = torch.clamp(prob_diff, min=0)
                         prob_diff = prob_diff / torch.sum(prob_diff)
                         next_generated_token = torch.multinomial(prob_diff, num_samples=1)
-                        # Update prompt tokens
                         prompt_tokens = torch.cat((prompt_tokens, next_generated_token), dim=0)
+                        n += 1
                         break
-                    n += 1
 
                 if all_accept:
-                    print("All accept!")
                     next_generated_token = torch.multinomial(target_model_probs[-1], num_samples=1)
-                    # Update prompt tokens
                     prompt_tokens = torch.cat([prompt_tokens, next_generated_token], dim=0)
                     n += 1
         generate_text = self.tokenizer.decode(prompt_tokens.tolist())
